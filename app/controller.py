77d774d9029c8bc9021bdf83b842b4b506439f87
ehickox2012 <ehickox2012@aol.com>
2019-07-26 21:35:12 -0700

    cleanup prod

--- app/controller.py
+++ app/controller.py
@@ -71,50 +71,10 @@ def get_last_years_commits():
 
 def get_projects():
     projects = []
-    projects.append(get_bitraider())
-    projects.append(get_bitscramblr())
     projects.append(get_vimdeploy())
     projects.append(get_pebble_bitcoin_wallet())
     projects.append(get_eshcript())
     return projects
-
-def get_bitscramblr():
-    bitscramblr = Project(title="bitscramblr", tagline="Bitcoin transaction anonymization",
-                          date="April, 2015", github="https://github.com/ehickox/bitscramblr",
-                          link="https://www.academia.edu/16612889/Bitcoin_Transaction_Anonymization_Through_Randomized_Dynamic_Transactions")
-
-    desc = ("Bitscramblr is an application that allows you to send Bitcoin anonymously. "
-            "It differs from many mixing services in existence in that a transaction will "
-            "not be sent until adequately sized inputs are available from another source. "
-            "Basically what this means is: there is no path from a transaction's origin to its destination. "
-            "For a more detailed overview of the Graph Theory principles that make Bitscramblr work, "
-            "click on 'Go to bitscramblr' to read my research paper. A proof of concept implementation can be "
-            "found at the GitHub link provided.")
-
-    bitscramblr.append_description(desc)
-    bitscramblr.add_license("Apache 2")
-    return bitscramblr
-
-def get_bitraider():
-    bitraider = Project(title="bitraider", tagline="A Bitcoin Algorithmic Trading Framework",
-                        date="May, 2015",
-                        github="https://github.com/ehickox/bitraider",
-                        link="http://ehickox.github.io/bitraider")
-
-    desc = ("Bitraider is a collection of tools for algorithmic bitcoin "
-           "trading in Python. Bitraider includes an Abstract Exchange "
-           "that allows it to be flexible as Bitcoin Exchanges come and go. "
-           "Currently, it uses CoinbaseExchange. Bitraider allows users to "
-           "create plug-n-play strategies using its Abstract Strategy. "
-           "Users can then backtest created strategies on historical data, "
-           "or optimize constant values in their strategies using cross-fold "
-           "validation before using Bitraider to perform live trades. For "
-           "more information on how to get started with bitraider, click on "
-           " the link below.")
-
-    bitraider.append_description(desc)
-    bitraider.add_license("MIT")
-    return bitraider
 
 def get_pebble_bitcoin_wallet():
     project = Project(title="BitcoinWallet",
