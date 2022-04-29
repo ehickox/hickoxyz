dfe520737339658e13b4aed30a8a8b9c9a393dcb
Eli Hickox <eli@elihickox.com>
2022-04-28 19:34:12 -0700

    reduce unused deps

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,7 @@
 from flask import Flask, url_for, Blueprint
 import os
-import chartkick
 
 app = Flask(__name__)
-ck = Blueprint(
-    "ck_page", __name__, static_folder=chartkick.js(), static_url_path="/static"
-)
-app.register_blueprint(ck, url_prefix="/ck")
-app.jinja_env.add_extension("chartkick.ext.charts")
 app.config["DEBUG"] = False
 app.config["SECRET_KEY"] = "SECRET_KEY_CH1ng3me"
 
