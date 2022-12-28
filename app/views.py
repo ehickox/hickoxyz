a66a2ad0ae4b15569e67efde1e6cc0f03f966c7d
Eli Hickox <eli@elihickox.com>
2022-12-28 10:26:28 -0800

    feat(hickoxyz): add custom page titles and a link to ehlabs blog

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect
 from app import app
 from app.controller import get_projects, get_last_years_commits, get_works
 
@@ -10,16 +10,21 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",
+                            title="about - elihickox.com")
 
 
 @app.route("/projects")
 def projects():
     projects_list = get_projects()
-    return render_template("projects.html", projects=projects_list)
+    return render_template("projects.html", projects=projects_list, title="projects - elihickox.com")
 
 
 @app.route("/works")
 def works():
     projects_list = get_works()
-    return render_template("works.html", projects=projects_list)
+    return render_template("works.html", projects=projects_list, title="collected works - elihickox.com")
+
+@app.route("/blog")
+def blog():
+    return redirect("https://www.ehlabs.net/blog/u/eli", code=302)
