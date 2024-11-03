1a32439263c1ebb16dae4dd39af75268fcd17188
ehickox <eli@elihickox.com>
2024-11-03 09:24:54 -0800

    feat(photos): swipe with photo carousel

--- app/static/js/slideshow.js
+++ app/static/js/slideshow.js
@@ -1,14 +1,17 @@
 let slideIndex = 0;
+let slideTimeout; // Variable to store the timeout
+let touchStartX = 0;
+let touchEndX = 0;
+
 showSlidesAuto();
-let intervalId = setInterval(showSlidesAuto, 4000); // Change image every 6 seconds
 
 function plusSlides(n) {
+  clearTimeout(slideTimeout); // Clear existing timeout
   showSlides(slideIndex += n);
-  clearInterval(intervalId);
-  intervalId = setInterval(showSlidesAuto, 4000); // Change image every 6 seconds
 }
 
 function currentSlide(n) {
+  clearTimeout(slideTimeout); // Clear existing timeout
   showSlides(slideIndex = n);
 }
 
@@ -26,11 +29,22 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  startSlideTimeout(); // Start the timeout again after slide change
+}
+
+function startSlideTimeout() {
+  clearTimeout(slideTimeout); // Clear existing timeout
+  slideTimeout = setTimeout(function() {
+    plusSlides(1); // Move to the next slide after the timeout
+  }, 4000); // Change image every 4 seconds
 }
 
 function showSlidesAuto() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  if (slides.length == 0) {
+    return;
+  }
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -42,5 +56,28 @@ function showSlidesAuto() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  slides[slideIndex-1].style.display = "block";
-} 
+  startSlideTimeout(); // Start the initial timeout
+}
+
+// Touch event handlers for swiping on images
+let images = document.querySelectorAll(".mySlides img");
+
+images.forEach(image => {
+  image.addEventListener('touchstart', function(event) {
+    touchStartX = event.touches[0].clientX;
+  }, false);
+
+  image.addEventListener('touchend', function(event) {
+    touchEndX = event.changedTouches[0].clientX;
+    handleSwipe();
+  }, false);
+});
+
+function handleSwipe() {
+  if (touchStartX - touchEndX > 50) {
+    plusSlides(1); // Swipe left, move to the next slide
+  } else if (touchEndX - touchStartX > 50) {
+    plusSlides(-1); // Swipe right, move to the previous slide
+  }
+}
+
