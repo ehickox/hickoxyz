618ed7c316600cc90ac5bd77e4fb1a60a3818902
Eli Hickox <eli@elihickox.com>
2025-10-12 12:44:29 -0700

    chore: scale down workers

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,8 @@
-import multiprocessing
+#import multiprocessing
 
 bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
+#workers = multiprocessing.cpu_count() * 2 + 1
+workers = 2
 
 # Choose one as appropriate.
 # worker_class = "sync"
